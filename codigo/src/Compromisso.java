ba39fa27ba95d0aa286d0188512f0dbf7c3de7a2
Lucas Santos <lucassr614@gmail.com>
2022-08-31 12:28:53 -0300

    update documentação das classes Compromisso e DataC

--- codigo/src/Compromisso.java
+++ codigo/src/Compromisso.java
@@ -2,28 +2,59 @@ import java.text.ParseException;
 
 public class Compromisso {
 
-
+    //#region Atributos
     private static String nome;
     private static DataC dataCompromisso;
 
+    //#endregion
+
+
+    //#region Construtores
+
+    /**
+     * Inicializa o objeto compromisso a Partir de uma String nome e uma String com a data do compromisso.
+     *
+     * @param name            Espera uma String para atribuir ao objeto compromisso
+     * @param dataCompromisso Espera uma String no formato dd/MM/yyyy para atribuir ao objeto compromisso
+     */
     public Compromisso(String name, String dataCompromisso) throws ParseException {
         init(name, dataCompromisso);
     }
 
+    /**
+     * Inicializa o objeto compromisso com o valor de nome "Não Iniciado" e a Data "01/01/2022"
+     */
     public Compromisso() throws ParseException {
         init("Não Iniciado", "01/01/2022");
     }
 
+    //#endregion
+
+
+    //region Métodos
+
+
+    /**
+     * Método privado utilizado para auxiliar na atribuição dos construtores
+     *
+     * @param nome            Espera uma String para atribuir o nome.
+     * @param dataCompromisso Espera uma String no formato dd/MM/yyyy.
+     * @throws ParseException Lança uma excessão caso o formato não corresponda ao esperado.
+     */
     private void init(String nome, String dataCompromisso) throws ParseException {
         Compromisso.nome = nome;
         Compromisso.dataCompromisso = new DataC(dataCompromisso);
     }
 
+    /**
+     * Método utilizado para imprimir na tela, os dados do compromisso no formato String
+     */
     public void imprimirCompromisso() {
         System.out.println(nome);
-        System.out.println(dataCompromisso.getData());
-
+        System.out.println(dataCompromisso.dataEmString());
     }
+    //#endregion
+
     //#Region Getters
 
     public String getNome() {
@@ -33,4 +64,6 @@ public class Compromisso {
     public DataC getDataCompromisso() {
         return dataCompromisso;
     }
+
+    //#endregion
 }
--- codigo/src/DataC.java
+++ codigo/src/DataC.java
@@ -2,28 +2,68 @@ import java.util.*;
 import java.text.*;
 
 public class DataC {
+
+    //#region Atributos
     private Date data;
 
+    //#endregion
+
+    //#region Construtores
+
+
+    /**
+     *  Inicializa o objeto Data, a partir da String com a data correspondente
+     * @param inputUsuario Espera uma String no formato dd/MM/yyyy
+     * @throws ParseException Lança excessão caso o formato do input esteja fora do padrão dd/MM/yyyy
+     */
     DataC(String inputUsuario) throws ParseException {
         init(inputUsuario);
     }
 
+
+    /**
+     * Inicializa o objeto data com o valor "01/01/2022 caso não seja passado nenhum parâmetro"
+     */
     DataC() throws ParseException {
         init("01/01/2022");
     }
 
+    //#endregion
+
+
+
+    //#region Métodos
+
+    /**
+     *  Método privado utilizado para iniciar os construtores
+     * @param inputUsuario Espera uma String no formato "dd/MM/yyyy"
+     * @throws ParseException Lança excessão caso o formato do input esteja fora do padrão dd/MM/yyyy
+     */
     private void init(String inputUsuario) throws ParseException {
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         this.data = sdf1.parse(inputUsuario);
     }
 
+    /**
+     * Método que converte a variavel date para String
+     * @return Retorna uma String no formato "dd/MM/yyyy"
+     */
     public String dataEmString() {
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.format(this.data);
     }
+
+    //#endregion
+
+
+
+    //#region Getters & Setters
+
     public Date getData() {
         return data;
     }
+
+    //#endregion
 
 }
 
